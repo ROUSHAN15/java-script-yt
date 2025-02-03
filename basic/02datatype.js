89d36ea7fd03950a80dce9a4967be8140477d578
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-03 00:02:42 +0000

    datatype

---
+++ basic/02datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //ytreat all js code as newer version 
+
+// alert("hello")  we are using node js not browser
+console.log(3+3);
+console.log("roushan");
+let name="roushan"
+let age="18"
+let isLoggedIn=false
+let state
+
+// number => 2 pow 63
+// bigint 
+// string => ""
+// boolean => true/ flase
+// null =>standalone value
+// undefined =>
+// symbol  =>uniquness
+//object=>imp......
+
+console.log(typeof "roushan"); //string
+
+console.log(typeof age)  //string
+console.log(typeof null)  // object
+console.log(typeof NULL)  // undefned
+console.log(typeof undefined) //undefned
