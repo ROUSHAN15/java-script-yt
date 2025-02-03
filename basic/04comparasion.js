a51f31fc3d5d24af0439a7d6d8aaea277db4c4c9
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-03 00:35:37 +0000

    comparasion

---
+++ basic/04comparasion.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null==0);   // for == the null operates diffrently
+// console.log(null>0);    //for >,<,=>,<= the null operates diffrently treates null as 0
+// console.log(null>=0);   
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+// console.log(undefined<-0);
+
+
+//        ===
+// strict comparision
+console.log("2"===2);
+
+
+
