30d86079046d2f4aca429eeee6d08150d7a9ee2d
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-03 00:02:57 +0000

    conversion

---
+++ basic/03conversion.js
@@ -0,0 +1,33 @@
+let score="33" 
+// let score1=null
+// let score2=true
+// let score3=undefined
+// let score="33abx"  // not a number
+
+// 33 => 33
+// 33abc =>NaN
+//true =>1 flase=>0
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+
+// console.log( valueInNumber)
+
+let isLoggedIn=11
+
+let boolean=Boolean(isLoggedIn)
+console.log(isLoggedIn);
+console.log(boolean);
+
+
+// 1=> true
+//0=> flase
+// "roushan=> true"
+
+let someNumber=69
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
