18a5c92175b5c06ba4006d47ae4264447ed0be97
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-03 18:10:52 +0000

    num and maths

--- basic/07NumANDMaths.js
+++ basic/07NumANDMaths.js
@@ -1,12 +1,8 @@
 const score=400
 console.log(score);
-
 const balance=new Number(100)
-
 console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
-
-
 const otherNumber=123.23547
 console.log(otherNumber.toPrecision());
