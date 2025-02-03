38a4ec19d4144ee9e01a68f3f466dd8ec86ac23d
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-03 18:46:36 +0000

    math and num

--- basic/07NumANDMaths.js
+++ basic/07NumANDMaths.js
@@ -1,8 +1,27 @@
-const score=400
-console.log(score);
-const balance=new Number(100)
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
-const otherNumber=123.23547
-console.log(otherNumber.toPrecision());
+// const score=400
+// console.log(score);
+// const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otherNumber=123.23547
+// console.log(otherNumber.toPrecision());
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++maths++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,6,8,0,-1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random(0,6)*10));
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
