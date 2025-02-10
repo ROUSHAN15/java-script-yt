c543d70f2e3a58f21d001598588da50cdecb85cc
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-10 13:57:23 +0000

    object2

---
+++ basic2/object2.js
@@ -0,0 +1,53 @@
+const tinderUser={}
+tinderUser.id="123abc"
+tinderUser.name="sammy"
+tinderUser.isLOGin=false
+
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{ userFULLname:{
+        firstname:"roushan",
+        lastname:"kumar",
+    }
+}
+}
+// console.log(regularUser.fullname.userFULLname.lastname);
+const obj1={1: "a",2: "b"}
+const obj2={3: "a",4: "b"}
+// const obj3={obj1,obj2}  // bad practice becausee it can do chnage in object 1
+// const obj3=Object.assign({},obj1,obj2)  // good practice 
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+const users =  [
+    
+    {
+        id: 1,
+        email: "roushan@mail.com"
+    },
+    {
+        id: 1,
+        email: "roushan@mail.com"
+    },
+    {
+        id: 1,
+        email: "roushan@mail.com"
+    }
+]
+
+
+console.log(users[0].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLOGin'))
+console.log(tinderUser.hasOwnProperty('isLOGi'))
+
+
+
