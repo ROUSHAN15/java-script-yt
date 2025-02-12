c93e97895ff764b43d91d05fd6f684e23f5b63f3
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-12 12:56:35 +0000

    forin

---
+++ iteration/forin.js
@@ -0,0 +1,27 @@
+// const myOBJECT={
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: 'rubby',
+//     swift: 'swift'
+// }
+
+// for (const key in myOBJECT) {
+//     console.log(key," -->>  ", `${myOBJECT[key]}`);
+// }
+
+// const programinng=["js","cpp","java","nodeJS","mern"]
+// for (const key in programinng) {
+//     console.log(programinng[key]);
+// }
+
+
+// const map=new Map()
+// map.set('IN',"india")
+// map.set('AUS',"austrila")
+// map.set('pak',"chutia")
+// map.set('ne',"nepal")
+
+// for (const  key in map) {      
+//             console.log(key);    /// non iratatable 
+            
+// }
