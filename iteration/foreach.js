8db148a33527e88e0bc1edb76aae956ab66c0663
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-12 13:41:01 +0000

    foreach

---
+++ iteration/foreach.js
@@ -0,0 +1,54 @@
+const coding = ["js","rubby","java","rubby","cpp"]
+
+// coding.forEach(function (item) {
+//     console.log(item);
+    
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+
+
+// function printME(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printME)
+
+// coding.forEach((item,index,arr)=>{
+//       console.log(item,index,arr);
+             
+// })
+
+const myCODING=
+[
+    {
+    languageNAME:"javascript",
+    langFILEnamse:"cpp"
+    },
+    {
+        languageNAME:"java",
+        langFILEnamse:"python"
+    },
+        
+    {
+            languageNAME:"ruby",
+            langFILEnamse:"cpp"
+     },
+    {
+                languageNAME:"ruby",
+                langFILEnamse:"react"
+    },
+    {
+                    languageNAME:"mysql",
+                    langFILEnamse:"cpp"
+    },
+                ]
+
+myCODING.forEach((item)=>{
+    console.log(item.languageNAME);
+    
+})
