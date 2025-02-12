b1a160fc3abc8e7824dbf8de518bf697f1b95d38
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-12 10:53:49 +0000

    forloop

---
+++ iteration/forlopp.js
@@ -0,0 +1,29 @@
+// for
+
+// for (let i = 0; i <10; i++) {
+//     const element = i;   
+//     console.log(element);
+
+// }
+
+
+// for (let i = 0; i <=3; i++) {
+//     console.log(`outher loop :  ${i}`)
+//     for (let j = 0; j <=4; j++) {
+//         const element = j;
+//         console.log(`outer loop value ${i}inner loop value ${j}`);
+        
+//     }
+    
+// }
+
+// break an continue
+
+for (let i = 0; i < 20; i++) {
+    if (i===5 || i===4) {
+        continue
+    }
+console.log(`value is ${i}`);
+if(i === 6)
+    break
+}
