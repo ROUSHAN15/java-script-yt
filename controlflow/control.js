49ab7b380ff2e4de81a56f94dfeb717e948b4b10
ROUSHAN15 <roushankkverma@gmail.com>
2025-02-12 09:49:22 +0000

    control1

---
+++ controlflow/control.js
@@ -0,0 +1,86 @@
+//if
+
+// const  isuserLogin=true
+ 
+
+// if(2==="2")
+// {
+//     console.log("executed");
+// }
+
+// === typecheking 
+//!== 
+// const temperature =41
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("temp bauth hai");
+
+// }
+
+// const score=200
+// if(score>100)
+// {   
+//     // var don use this
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// const balance =1000
+// // if (balance>500) console.log("test"),console.log("test2");
+// if(balance<500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance>500)
+//     {
+//         console.log("more than 500");
+//     }
+//     else{
+//         console.log("less than 1200");
+//     }
+
+// const userLOGGin=true
+// const debitCard=true
+// const LOGinFROMgoogle=false
+// const  emailLOGIN=true
+// if(userLOGGin && debitCard && (LOGinFROMgoogle || emailLOGIN))
+// {
+//     console.log("Allow to buy cources");
+// }
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month =3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+        
+    case 2:
+        console.log("feb");
+        break;
+        
+    case 3:
+        console.log("mar");
+        break;
+        
+    case 4:
+        console.log("apr");
+        break;
+        
+
+    default:
+        break;
+}
